043171c299370a9825593bdc33c1679622dd08b1
Marcos Lima <mrsk1414@gmail.com>
2026-04-23 06:58:19 -0300

    fix: increase getLinkPreview timeout to 10000ms

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -33,7 +33,7 @@ export async function createNewsService(
           "user-agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         },
-        timeout: 5000,
+        timeout: 10000,
       })) as Preview;
 
       if (!newData.images[0]) continue;
